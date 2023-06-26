b122f6bfa194268caea2dac184f9a2f6f0ee032c
else <15102082382@163.com>
2023-06-26 17:55:55 +0800

    fix：20893 【功能优化】体温单表底注释“不在”时，体温单连线需要隔断，20942 【功能优化】PC端同一时间点的多个表顶注释顺序调整

--- src/projects/zzwy/mockData.js
+++ src/projects/zzwy/mockData.js
@@ -1,140 +1,205 @@
-export const mockData = {
-    "ErrorCode": "1",
-    "ErrorMsg": "查询成功！",
-    "patientInfo": {
-        "patInfo": {
-            "patient_id": "0000020466",
-            "name": "王素卿",
-            "dept_name": "外科病区",
-            "bed_label": null,
-            "visit_id": "2",
-            "admission_date": "2023-04-18 09:25:20",
-            "age": "76岁",
-            "inp_no": "170816",
-            "sex": "女",
-            "birthday": "1946-09-19 00:00:00.0"
-        }
-    },
-    "vitalSigns": [{
-        "temperature_type": "表顶注释",
-        "value": "手术",
-        "time_point": "2023-04-18 15:00:00",
-        "vital_code": "5",
-        "expand2": "2023-04-18 16:08:00"
-    }, {
-        "temperature_type": "表顶注释",
-        "value": "手术",
-        "time_point": "2023-05-09 15:00:00",
-        "vital_code": "5",
-        "expand2": "2023-05-09 15:31:00"
-    }, {
-        "temperature_type": "脉搏",
-        "value": "30",
-        "time_point": "2023-05-11 15:00:00",
-        "vital_code": "02",
-        "expand2": null
-    }, {
-        "temperature_type": "呼吸",
-        "value": "25",
-        "time_point": "2023-04-19 15:00:00",
-        "vital_code": "04",
-        "expand2": null
-    }, {
-      "temperature_type": "呼吸",
-      "value": "23",
-      "time_point": "2023-04-20 11:00:00",
-      "vital_code": "04",
-      "expand2": null
-    }, {
-      "temperature_type": "呼吸",
-      "value": "22",
-      "time_point": "2023-04-21 11:00:00",
-      "vital_code": "04",
-      "expand2": null
-    },{
-      "temperature_type": "呼吸",
-      "value": "22",
-      "time_point": "2023-04-22 11:00:00",
-      "vital_code": "04",
-      "expand2": null
-    }, {
-      "temperature_type": "呼吸机",
-      "value": "R",
-      "time_point": "2023-04-19 15:00:00",
-      "vital_code": "06",
-      "expand2": null
-    }, {
-      "temperature_type": "呼吸机",
-      "value": "R",
-      "time_point": "2023-04-20 11:00:00",
-      "vital_code": "06",
-      "expand2": null
-    }, {
-      "temperature_type": "呼吸机",
-      "value": "R",
-      "time_point": "2023-04-23 11:00:00",
-      "vital_code": "06",
-      "expand2": null
-    },{
-      "temperature_type": "呼吸机",
-      "value": "R",
-      "time_point": "2023-04-24 11:00:00",
-      "vital_code": "06",
-      "expand2": null
-    }, {
-        "temperature_type": "表顶注释",
-        "value": "手术",
-        "time_point": "2023-05-11 15:00:00",
-        "vital_code": "5",
-        "expand2": "2023-05-11 15:31:00"
-    }, {
-        "temperature_type": "心率",
-        "value": "63",
-        "time_point": "2023-05-11 15:00:00",
-        "vital_code": "20",
-        "expand2": null
-    }, {
-        "temperature_type": "表顶注释",
-        "value": "手术",
-        "time_point": "2023-05-14 11:00:00",
-        "vital_code": "5",
-        "expand2": "2023-05-14 10:48:00"
-    }, {
-        "temperature_type": "发热",
-        "value": "38.5",
-        "time_point": "2023-04-18 19:18:20",
-        "vital_code": "05",
-        "expand2": null
-    }, {
-        "temperature_type": "呼吸",
-        "value": "20",
-        "time_point": "2023-04-18 19:18:20",
-        "vital_code": "04",
-        "expand2": null
-    }, {
-        "temperature_type": "皮试",
-        "value": "达到当地达到当地(+)",
-        "time_point": "2023-05-15 11:00:00",
-        "vital_code": "30",
-        "expand2": null
-    }, {
-        "temperature_type": "体温",
-        "value": "36.7",
-        "time_point": "2023-04-18 08:18:20",
-        "vital_code": "01",
-        "expand2": null
-    }, {
-        "temperature_type": "物理降温",
-        "value": "37.6",
-        "time_point": "2023-05-15 11:00:00",
-        "vital_code": "3",
-        "expand2": null
-    }, {
-        "temperature_type": "表顶注释",
-        "value": "入院|九时二十五分",
-        "time_point": "2023-04-18 08:18:20",
-        "vital_code": "5",
-        "expand2": null
-    }]
-}
+export const mockData =
+    {
+        "ErrorCode": "1",
+        "ErrorMsg": "查询成功！",
+        "patientInfo": {
+            "patInfo": {
+                "patient_id": "0000037676",
+                "name": "陈美玉",
+                "dept_name": "外科病区",
+                "bed_label": "15",
+                "visit_id": "1",
+                "admission_date": "2023-06-16 15:52:45",
+                "age": "73岁",
+                "inp_no": "0000037676",
+                "sex": "女",
+                "birthday": "1949-11-09 00:00:00.0"
+            }
+        },
+        "vitalSigns": [
+            {
+                "temperature_type": "呼吸",
+                "value": "20",
+                "time_point": "2023-06-16 14:00:00",
+                "vital_code": "04",
+                "expand2": null
+            },
+            {
+                "temperature_type": "脉搏",
+                "value": "78",
+                "time_point": "2023-06-16 14:00:00",
+                "vital_code": "02",
+                "expand2": null
+            },
+            {
+                "temperature_type": "身高",
+                "value": "138",
+                "time_point": "2023-06-16 14:00:00",
+                "vital_code": "094",
+                "expand2": null
+            },
+            {
+                "temperature_type": "疼痛",
+                "value": "0",
+                "time_point": "2023-06-16 14:00:00",
+                "vital_code": "092",
+                "expand2": null
+            },
+            {
+                "temperature_type": "体温",
+                "value": "36.5",
+                "time_point": "2023-06-16 14:00:00",
+                "vital_code": "01",
+                "expand2": null
+            },
+            {
+                "temperature_type": "体重",
+                "value": "45",
+                "time_point": "2023-06-16 14:00:00",
+                "vital_code": "033",
+                "expand2": null
+            },
+            {
+                "temperature_type": "血压",
+                "value": "220/108",
+                "time_point": "2023-06-16 14:00:00",
+                "vital_code": "062",
+                "expand2": null
+            },
+            {
+                "temperature_type": "表底注释",
+                "value": "1不在",
+                "time_point": "2023-06-17 06:00:00",
+                "vital_code": "4",
+                "expand2": null
+            },
+            {
+                "temperature_type": "疼痛",
+                "value": "0",
+                "time_point": "2023-06-17 10:00:00",
+                "vital_code": "092",
+                "expand2": null
+            },
+            {
+                "temperature_type": "表底注释",
+                "value": "2不在",
+                "time_point": "2023-06-17 14:00:00",
+                "vital_code": "4",
+                "expand2": null
+            },
+            {
+                "temperature_type": "表底注释",
+                "value": "3不在",
+                "time_point": "2023-06-18 22:00:00",
+                "vital_code": "4",
+                "expand2": null
+            },
+            {
+                "temperature_type": "表底注释",
+                "value": "4不在",
+                "time_point": "2023-06-19 06:00:00",
+                "vital_code": "4",
+                "expand2": null
+            },
+            {
+                "temperature_type": "表底注释",
+                "value": "5不在",
+                "time_point": "2023-06-19 14:00:00",
+                "vital_code": "4",
+                "expand2": null
+            },
+            {
+                "temperature_type": "表底注释",
+                "value": "6不在",
+                "time_point": "2023-06-19 18:00:00",
+                "vital_code": "4",
+                "expand2": null
+            },
+            {
+                "temperature_type": "表底注释",
+                "value": "不在",
+                "time_point": "2023-06-20 13:00:00",
+                "vital_code": "4",
+                "expand2": null
+            },
+            {
+                "temperature_type": "体温",
+                "value": "36.7",
+                "time_point": "2023-06-20 10:00:00",
+                "vital_code": "01",
+                "expand2": null
+            },
+            {
+                "temperature_type": "脉搏",
+                "value": "76",
+                "time_point": "2023-06-20 10:00:00",
+                "vital_code": "02",
+                "expand2": null
+            },
+            {
+                "temperature_type": "呼吸",
+                "value": "19",
+                "time_point": "2023-06-20 10:00:00",
+                "vital_code": "04",
+                "expand2": null
+            },
+            {
+                "temperature_type": "血压",
+                "value": "213/96",
+                "time_point": "2023-06-20 10:00:00",
+                "vital_code": "062",
+                "expand2": null
+            },
+            {
+                "temperature_type": "表顶注释",
+                "value": "手术",
+                "time_point": "2023-06-20 14:00:00",
+                "vital_code": "5",
+                "expand2": "2023-06-20 14:17:00"
+            },
+            {
+                "temperature_type": "大便",
+                "value": "1",
+                "time_point": "2023-06-20 14:00:00",
+                "vital_code": "061",
+                "expand2": null
+            },
+            {
+                "temperature_type": "呼吸",
+                "value": "20",
+                "time_point": "2023-06-20 14:00:00",
+                "vital_code": "04",
+                "expand2": null
+            },
+            {
+                "temperature_type": "脉搏",
+                "value": "69",
+                "time_point": "2023-06-20 14:00:00",
+                "vital_code": "02",
+                "expand2": null
+            },
+            {
+                "temperature_type": "疼痛",
+                "value": "0",
+                "time_point": "2023-06-20 14:00:00",
+                "vital_code": "092",
+                "expand2": null
+            },
+            {
+                "temperature_type": "体温",
+                "value": "36.7",
+                "time_point": "2023-06-20 14:00:00",
+                "vital_code": "01",
+                "expand2": null
+            },
+            {
+                "temperature_type": "表顶注释",
+                "value": "入院|十五时五十二分",
+                "time_point": "2023-06-16 15:52:45",
+                "vital_code": "5",
+                "expand2": null
+            }
+        ]
+    }
+
 export default {}
