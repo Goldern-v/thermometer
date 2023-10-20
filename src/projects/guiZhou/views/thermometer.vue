656e5d0772928f61ca5235c96175c2cd1df750ff
Y-zone <2608347103@qq.com>
2023-10-20 09:03:43 +0800

    补提

--- src/projects/guiZhou/views/thermometer.vue
+++ src/projects/guiZhou/views/thermometer.vue
@@ -488,7 +488,7 @@ export default {
     const pulseRange = [20, 180];
     const painRange = [0, 10];
     return {
-      useMockData: true,
+      useMockData: false,
       apiData: "", // 接口数据
       zr: "",
       showFlage: true,
